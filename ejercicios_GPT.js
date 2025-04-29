0f4422789ddca08b7d3c8c4bc6dc492a560f8bd3
STVNIU <stvntico@gmail.com>
2025-04-29 11:52:31 -0500

    Add files via upload

---
+++ ejercicios_GPT.js
@@ -0,0 +1,32 @@
+/* 
+Crea un array llamado colores con los siguientes valores: "rojo", "verde", "azul".
+Luego:
+
+Agrega "amarillo" al final.
+
+Elimina el primer color.
+
+Muestra todos los colores en consola.
+*/
+
+const colors = ["red" , "green", "blue"];
+/* colors.push("yellow");
+colors.shift();
+console.log(colors); */
+
+
+/* let coloresMayusculas = colors.map(color => color.toUpperCase());
+console.log(coloresMayusculas);
+*/
+
+let numbers = [10, 15, 20, 25, 30];
+/* let numeroNumbers= numbers.filter(number => number > 20);
+console.log(numeroNumbers); */
+let firstNumber = numbers.find(number => number > 15);
+console.log(firstNumber)
+
+
+
+
+
+
