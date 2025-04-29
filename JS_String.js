02071a84b6046a75285d6237e7caca0ac9c78680
STVNIU <stvntico@gmail.com>
2025-04-29 09:02:53 -0500

    Add files via upload

---
+++ JS_String.js
@@ -0,0 +1,8 @@
+let texto = "Hola Mundo";
+console.log(texto.length);
+console.log(texto.toUpperCase());
+console.log(texto.toLowerCase());
+console.log(texto.includes("Mundo"));
+console.log(texto.slice(1 , 4));
+console.log(texto.replace("Mundo" , "JS"));
+console.log(texto.split(" "));
